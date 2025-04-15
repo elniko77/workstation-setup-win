e4d0ca59ba76ee1753eb1e27890bfd1f7a52f39c
nss <nico77@gmail.com>
2025-04-15 11:12:23 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,7 +3,6 @@ Disable-UAC
 
 # Install git and clone repository containing scripts and config files
 winget install --silent --accept-package-agreements and --accept-source-agreements install Microsoft.Git
-RefreshEnv
 git clone https://github.com/elniko77/workstation-setup-win.git "$env:USERPROFILE\workstation-setup-win"
 
 Enable-UAC
