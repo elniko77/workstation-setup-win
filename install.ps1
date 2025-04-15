04598aa1d54ea3acdb2e3bc5af03e169ed4580df
Nicolas Sosa (nss) <nico77@gmail.com>
2025-04-15 11:36:41 -0300

    call prefs

--- install.ps1
+++ install.ps1
@@ -9,6 +9,8 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 
 git clone https://github.com/elniko77/workstation-setup-win.git "$env:USERPROFILE\workstation-setup-win"
 
+Set-ExecutionPolicy Unrestricted -Force
+
 # Set windows desktop preferences (theme, file explorer, etc)
 . "$env:USERPROFILE\workstation-setup-win\scripts\winpreferences.ps1"
 
--- scripts/winpreferences.ps1
+++ scripts/winpreferences.ps1
@@ -8,4 +8,4 @@ Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtec
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0
 # Set wallpaper
-Set-Itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaper -value "$env:USERPROFILE\workstation-setup-win\files\wallpaper.png
+Set-Itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaper -value "$env:USERPROFILE\workstation-setup-win\files\wallpaper.png"
