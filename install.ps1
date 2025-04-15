dee11aa6f4531a04a5d247e4242965cdcc1651c2
nss <nico77@gmail.com>
2025-04-15 11:10:44 -0300

    Update install.ps1
    Silent winget install options

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 Disable-UAC
 
 # Install git and clone repository containing scripts and config files
-winget install -e -h --id install -y Microsoft.Git
+winget install --silent --accept-package-agreements and --accept-source-agreements install Microsoft.Git
 RefreshEnv
 git clone https://github.com/elniko77/workstation-setup-win.git "$env:USERPROFILE\workstation-setup-win"
 
