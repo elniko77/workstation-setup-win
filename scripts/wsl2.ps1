4091a1bf43b48ce33715d9c21ff0737153058fc6
Nicolas Sosa (nss) <nico77@gmail.com>
2025-04-15 14:16:58 -0300

    wsl2

--- scripts/wsl2.ps1
+++ scripts/wsl2.ps1
@@ -1,6 +1,10 @@
+
+dism.exe /online /enable-feature /featurename:Microsoft-Hyper-V-All /featurename:VirtualMachinePlatform /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
 
 wsl --install
 
