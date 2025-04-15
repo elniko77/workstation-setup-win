5092e2378a930a390aedb804592bd68ec6315684
Nicolas Sosa (nss) <nico77@gmail.com>
2025-04-15 11:31:44 -0300

    call prefs

--- install.ps1
+++ install.ps1
@@ -9,4 +9,8 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 
 git clone https://github.com/elniko77/workstation-setup-win.git "$env:USERPROFILE\workstation-setup-win"
 
+# Set windows desktop preferences (theme, file explorer, etc)
+. "$env:USERPROFILE\workstation-setup-win\scripts\winpreferences.ps1"
 
+# Install applications
+#. "$env:USERPROFILE\workstation-setup-win\scripts\apps.ps1"
